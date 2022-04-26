4288870d37fefa7f805358ffeadea1e5eb36203d
pointehd <pointehd@naver.com>
2022-04-26 23:59:39 +0900

    function utils 테스트

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -13,3 +13,20 @@ export default class DateUtils {
     return [year, month, day].join('-');
   }
 }
+/**
+ * 
+ * function Utils
+ export const dateToString: Function = (date: Date) => {
+  let d = new Date(date);
+  let month = (d.getMonth() + 1).toString();
+  let day = d.getDate().toString();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+  return [year, month, day].join('-');
+};
+ */
