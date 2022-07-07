99b5ce34b945d19f8410350946f30f04076a612b
sasumpi123 <sasumpi123@naver.com>
2022-07-08 00:37:20 +0900

    마크다운 뷰어 세팅

---
+++ src/components/containers/Markdown/MarkdownEditor.tsx
@@ -0,0 +1,3 @@
+export const MarkdownEditor: React.FC = () => {
+  return <></>;
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,19 +1,36 @@
 import { NextPage } from 'next';
 import { Space } from 'antd';
-import ReactMarkDown from 'react-markdown';
-import content from '../../resources/contents/test.md';
+import { useEffect, useState, useCallback } from 'react';
+import { MarkdownViewer } from '../components/containers/Markdown';
 
+const md = `### Hello:raised_hand: My name is SungHoon and I'm Junior FE Developer.:baby:<br/>
+![sasumpi123's github stats](https://github-readme-stats.vercel.app/api?username=sasumpi123&show_icons=true)[![sasumpi123's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=sasumpi123&show_icons=true&hide_border=true&title_color=004386&icon_color=004386&layout=compact)](https://github.com/sasumpi123)
+**Languages and Tools**
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/5c058a388828bb5fde0bcafd4bc867b5bb3f26f3/topics/graphql/graphql.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png"></code>
+<code><img height="20" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"></code>
+`;
 const AboutPage: NextPage = () => {
-  const handle = async () => {};
+  const [value, setValue] = useState(md);
 
-  handle();
-
+  const handleChange = useCallback((value: any) => {
+    setValue(value);
+  }, []);
   return (
     <>
-      <Space>
-        <ReactMarkDown content={content} />
-      </Space>
-      <Space></Space>
+      <MarkdownViewer
+        value={value}
+        onChange={handleChange}
+        hideToolbar={true}
+        height={700}
+        preview={'preview'}
+        highlightEnable={false}
+      />
     </>
   );
 };
