909d86475251e5f6df6f359c365ff8eabaea5d5d
sasumpi123 <sasumpi123@naver.com>
2022-07-23 00:52:16 +0900

    set API_BASE_URL

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_GITHUB_AUTHORIZATION_TOKEN: string;
     GITHUB_AUTHORIZATION_TOKEN: string;
+    API_BASE_URL: string;
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  env: {
+    API_BASE_URL: 'https://api.joylog.co.kr/',
+  },
   async redirects() {
     return [
       {
